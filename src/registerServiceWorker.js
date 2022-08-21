773b90acb1434df9c3d1a84a4fd5c19af2dc3b85
CMEONE <contact@kabirwearsato.ga>
2022-08-21 03:15:55 -0700

    Revert service worker changes

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -5,8 +5,7 @@ import { register } from 'register-service-worker'
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     registrationOptions: {
-      scope: "/",
-      exclude: ["netlify.toml"]
+      scope: "/"
     },
     ready () {
       console.log(
